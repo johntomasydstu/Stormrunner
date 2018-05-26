b66f826dffab8de4e008f025f81e1d7291212282
johntomasydstu <john.toma@sydstu.catholic.edu.au>
2018-05-27 09:11:14 +1000

    1.4
    - Basic Main Menu Added

---
+++ Stormrunner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void playGame() 
+	{
+		SceneManager.LoadScene ("Game"); //Loads the "Game" scene
+	}
+
+	public void quitGame() 
+	{
+		Application.Quit (); //Quit Game
+	}
+}
